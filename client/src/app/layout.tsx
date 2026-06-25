c74d6b4c5e93dc79358b154ab0d2e53e4204c1b1
Jackson <jacksonjude97@gmail.com>
2026-06-25 04:39:27 -0400

    placeholder theme

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from "next";
-import { Geist } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const geist = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+  weight: "400",
 });
 
 export const metadata: Metadata = {
@@ -16,7 +22,10 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} antialiased`}>
+    <html
+      lang="en"
+      className={`${geist.variable} ${geistMono.variable} antialiased`}
+    >
       <body className="flex min-h-screen flex-col">{children}</body>
     </html>
   );
