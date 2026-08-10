f4858bcceab47a19ffd4e00011beb92fa1ce72b6
Jackson <jacksonjude97@gmail.com>
2026-08-09 21:33:12 -0400

    override autofill styles

--- client/src/components/ui/input.tsx
+++ client/src/components/ui/input.tsx
@@ -9,6 +9,7 @@ function Input({ type = "text", className, ...props }: InputPrimitive.Props) {
       className={cn(
         "border-border bg-surface text-foreground h-10 w-full rounded-md border p-2 text-sm",
         "placeholder:text-foreground-subtle disabled:pointer-events-none disabled:opacity-50 any-pointer-coarse:text-base",
+        "[&:-webkit-autofill]:caret-foreground [&:-webkit-autofill]:[-webkit-text-fill-color:var(--color-foreground)] [&:-webkit-autofill]:[transition:background-color_999999s_ease-out]",
         "focus:outline-focus-ring focus:outline-2 focus:-outline-offset-1",
         "data-invalid:outline-danger data-invalid:outline-2 data-invalid:-outline-offset-1",
         className,
