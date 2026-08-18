16a8582827c7d99b7b82bd46d141b084fd85af67
Jackson <jacksonjude97@gmail.com>
2026-08-18 04:56:29 -0400

    added a way to go back to the dashboard from landing page if valid session exists

--- client/src/app/(site)/layout.tsx
+++ client/src/app/(site)/layout.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { headers } from "next/headers";
+import { auth } from "@/lib/auth-client";
 
-function Header() {
+function Header({ isValidSession }: { isValidSession: boolean }) {
   return (
     <header>
       <nav className="flex justify-between gap-4 p-4">
@@ -11,8 +13,14 @@ function Header() {
         </div>
 
         <div className="flex gap-4">
-          <Link href="/login">Log In</Link>
-          <Link href="/signup">Get Started</Link>
+          {isValidSession ? (
+            <Link href="/dashboard">Dashboard</Link>
+          ) : (
+            <>
+              <Link href="/login">Log In</Link>
+              <Link href="/signup">Get Started</Link>
+            </>
+          )}
         </div>
       </nav>
     </header>
@@ -23,12 +31,18 @@ function Footer() {
   return <footer className="p-4">Footer</footer>;
 }
 
-export default function Layout({
+export default async function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const { data: session } = await auth.getSession({
+    fetchOptions: {
+      headers: await headers(),
+    },
+  });
+
   return (
     <>
-      <Header />
+      <Header isValidSession={!!session} />
       <main>{children}</main>
       <Footer />
     </>
