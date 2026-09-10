55a9b0160d6567480d4907bb8fb0d2021dd6a054
Jackson <jacksonjude97@gmail.com>
2026-09-10 03:02:32 -0400

    migrated to cn

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,13 +1,4 @@
-import { twMerge } from "tailwind-merge";
-import { clsx, type ClassValue } from "clsx";
-
-export const cx = (...inputs: ClassValue[]) => {
-  return clsx(inputs);
-};
-
-export const cn = (...inputs: ClassValue[]) => {
-  return twMerge(clsx(inputs));
-};
+export { cn } from "cn";
 
 export const formatBytes = (bytes: number, decimals = 2): string => {
   if (bytes === 0) return "0 Bytes";
